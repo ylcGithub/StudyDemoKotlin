8691db1a70f30abf834d68c8a80d87fea69265dd
ylc_home_computer <812097344@qq.com>
2022-04-30 23:32:27 +0800

    第一次提交

---
+++ app/src/main/java/ylc/kotlin/com/MainActivity.kt
@@ -0,0 +1,13 @@
+package ylc.kotlin.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
